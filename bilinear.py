b73eda85e186cba35f2c93f723c817ddd1d0d26c
rgmyr <ross.meyer@utexas.edu>
2018-07-11 02:18:32 -0600

    finish Encoding layer, first training tests

--- bilinear.py
+++ bilinear.py
@@ -16,6 +16,45 @@ TODO: support for matrix square root layer described in "Improved Bilinear Pooli
 import tensorflow as tf
 from keras import backend as K
 from keras import models, layers
+from keras.engine.topology import Layer
+
+
+def BilinearModel(Layer):
+    '''Weighted bilinear model of two inputs. Common use is to learn a model on the interactions
+       of seperate feature types (e.g., texture X spatial) or scales (dense X dilated).
+
+    #TODO: finish docstring
+    '''
+    
+    def __init__(self, **kwargs):
+        super(BilinearModel, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        if not isinstance(input_shape, list) or len(input_shape)!=2:
+            raise ValueError('Input to `BilinearModel` layer should be '
+                             'a list of exactly two inputs')
+        # TODO: enforce 2-D (batches, N) (batches, M)
+
+        self.shapeA, self.shapeB = input_shape[0][1], input_shape[1][1]
+        
+        self.weights = self.add_weight(name='outer_prod_weights',
+                                       shape=(self.shapeA, self.shapeB),
+                                       initializer='glorot_normal',
+                                       trainable=True)
+    def call(self, x):
+        if not isinstance(x, list) or len(x)!=2:
+            raise ValueError('A `BilinearModel` layer should be called '
+                             'on a list of exactly two inputs')
+        if K.int_shape(x[0])!=self.A or K.int_shape(x[1])!=self.B:
+            raise ValueError('Unexpected `BilinearModel` input_shapes')
+
+        weighted_outer = tf.multiply(self.W, tf.einsum('bi,bj->bij', x[0], x[1]))
+
+        return K.Flatten(weighted_outer)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], self.shapeA, self.shapeB)
+
 
 
 def pooling(inputs):
--- encoding.py
+++ encoding.py
@@ -7,84 +7,99 @@ Keras 2.0+ / TensorFlow implementation of learnable encoding layer, as proposed 
 '''
 import tensorflow as tf
 from keras import backend as K
+from keras.initializers import RandomUniform
 from keras.engine.topology import Layer
 
 
 def scaledL2(R, S): 
     ''' L2 norm over features of R, scaled by a codeword-length vector S.
     Args:
-        R (Tensor): 3-D tensor of shape (N,K,D)
+        R (Tensor): 4-D tensor of shape (batches,N,K,D) 
+                 or 3-D tensor of shape (N,K,D)
         S (Tensor): 1-D tensor of shape (K,)
 
     Returns:
         Tensor: L2 norm of R along D, scaled over K axis by S
-                Shape of output is (N,K)
+                Shape of output is (batches,N,K) or (N,K)
     '''
     l2_R = tf.linalg.norm(R, axis=-1)
     return tf.multiply(l2_R, S)
 
 
 class Encoding(Layer):
-    '''Residual encoding layer with learnable codebook and assignment weights.
+    '''Residual encoding layer with learnable codebook and scaling weights.
 
-    Encodes an collection of NxD features into a KxD representation
+    Encodes an collection of NxD features into set of KxD vectors.
 
-    Input of shape (None, H, W, D) from e.g., a conv layer, should be 
-    squeezed to (None, H*W, D) before feeding to Encoding layer.
+    Input of shape (batches, H, W, D) from e.g., a conv layer, should be 
+    collapsed to (batches, H*W, D) before feeding to Encoding layer. This
+    should not be necessary in a later release.
 
     Args:
-        D (int): dimensionality of features (and codewords)
         K (int): number of codewords to learn
+        dropout (float): dropout rate between [0.0,1.0) (default=`None`)
+        l2_normalize (bool): normalize output vectors (default=`True`)
 
     TODO: - support for dropout
           - support for (H, W, D) input
-          - better initialization params
+          - std computation + uniform initiliazation
     '''
-    def __init__(self, D, K, **kwargs):
+    def __init__(self, K, dropout=None, l2_normalize=True, **kwargs):
         super(Encoding, self).__init__(**kwargs)
-        self.D, self.K = D, K
+        self.K = K
+        self.l2_normalize = l2_normalize
+        self.dropout_rate = dropout
+
 
     def build(self, input_shape):
-        # TODO: implement std computation + uniform sampling
+        self.D = input_shape[-1]
 
+        std1 = 1./((self.K*self.D)**(0.5))
+        init_codes = RandomUniform(-std1, std1)
         self.codes = self.add_weight(name='codebook',
                                     shape=(self.K, self.D,),
-                                    initializer='orthogonal', # should use uniform +/-std1?
+                                    initializer=init_codes,
                                     trainable=True)
 
+        init_scale = RandomUniform(-1, 0)
         self.scale = self.add_weight(name='scale_factors',
                                     shape=(self.K,),
-                                    initializer='uniform',
+                                    initializer=init_scale,
                                     trainable=True)
 
         super(Encoding, self).build(input_shape)
 
     def call(self, x):
+        # Input is a 3-D or 4-D Tensor
         ndim = K.ndim(x)
-
-        # TODO: make this work (tuple error?)
-        #if ndim == 4:
-        #    dims = K.shape(x)
-        #    x = K.reshape(x, (-1, dims[1]*dims[2], self.D))
-
-        if ndim != 3:
-            raise ValueError('`x` should have shape BxNxD')
+        if ndim == 4:
+            dims = K.int_shape(x)
+            x = K.reshape(x, (-1, dims[1]*dims[2], self.D))
+        elif ndim != 3:
+            raise ValueError('Encoding input should have shape BxNxD or BxHxWxD')
         
         # Residual vectors
-        N = x.shape[1]
+        n = x.shape[1]
         _x_i = K.repeat_elements(x, self.K, 1)
-        _c_k = K.tile(self.codes, (N, 1))
-        R = K.reshape(_x_i - _c_k, (N, self.K, self.D)) 
-        
-        # Assignment weights
-        W_ik = K.softmax(scaledL2(R, self.scale))
+        _c_k = K.tile(self.codes, (n, 1))
+        R = K.reshape(_x_i - _c_k, (-1, n, self.K, self.D)) 
+
+        # Assignment weights, optional dropout
+        if self.dropout_rate is not None:
+            W_ik =  K.softmax(scaledL2(R, K.dropout(self.scale, self.dropout_rate)))
+        else:
+            W_ik = K.softmax(scaledL2(R, self.scale))
 
         # Aggregation
-        E = tf.einsum('ik,ikd->kd', W_ik, R)
+        E = tf.einsum('bik,bikd->bkd', W_ik, R)
+
+        # Normalize encoding vectors
+        if self.l2_normalize:
+            E = tf.nn.l2_normalize(E, axis=-1)
 
         return E
 
     def compute_output_shape(self, input_shape):
-        return (self.K, self.D)
-
+        # (batches, codeworks, dimensions)
+        return (input_shape[0], self.K, input_shape[-1])
 
