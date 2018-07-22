586a761187d3d3d77dd4c0c0b663642ab7aae806
rgmyr <ross.meyer@utexas.edu>
2018-07-22 16:51:46 -0600

    docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import texture
 
 
 # -- General configuration ------------------------------------------------
@@ -87,7 +88,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
